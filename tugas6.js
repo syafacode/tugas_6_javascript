669702aa8887a1c85667268d73f35a2518d7c2c4
Syafa Athaya Firstnora <syafacode.01@gmail.com>
2020-04-29 15:55:54 +0700

    bilangan ganjil genap

---
+++ tugas6.js
@@ -0,0 +1,11 @@
+function bilangan(){
+    for(var i = 1; i <= 20; i++){
+        if(i % 2 == 0){
+            console.log("bilangan genap")
+        }else{
+            console.log(i)
+        }
+    }
+}
+
+bilangan();
